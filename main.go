a530d0a346bd2bdbdc89ddccda2f12877630bc71
Manachai Sungwalsuwan <manachai@orange-thailand.com>
2022-07-22 16:08:37 +0700

    [Edit] posts api

--- main.go
+++ main.go
@@ -6,12 +6,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Cryptocurrency struct {
+	Id     int    `json:"id"`
+	Name   string `json:"name"`
+	Symbol string `json:"symbol"`
+	Rank   int    `json:"rank"`
+	Image  string `json:"image"`
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/posts", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "posts",
-		})
+		var cryptocurrencies = []Cryptocurrency{
+			{Id: 1, Name: "Bitcoin", Symbol: "BTC", Rank: 1, Image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1.png"},
+			{Id: 2, Name: "Ethereum", Symbol: "ETH", Rank: 2, Image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png"},
+			{Id: 3, Name: "Tether", Symbol: "USDT", Rank: 3, Image: "https://s2.coinmarketcap.com/static/img/coins/64x64/825.png"},
+			{Id: 4, Name: "USD Coin", Symbol: "USDC", Rank: 4, Image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png"},
+			{Id: 5, Name: "BNB", Symbol: "BNB", Rank: 5, Image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png"},
+		}
+		c.JSON(http.StatusOK, cryptocurrencies)
 	})
 	r.Run()
 }
